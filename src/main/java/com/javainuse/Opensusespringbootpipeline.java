c9a091add9b538a2842df8a2e412907e5a530b3e
ravigrv21290 <40420261+ravigrv21290@users.noreply.github.com>
2019-07-05 11:40:39 +0530

    Update Opensusespringbootpipeline.java

--- src/main/java/com/javainuse/Opensusespringbootpipeline.java
+++ src/main/java/com/javainuse/Opensusespringbootpipeline.java
@@ -1,7 +1,7 @@
 package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.opensusespringbootpipeline;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class opensusespringbootpipeline {
